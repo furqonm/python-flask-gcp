d4ec567bbcbc0a52cad65839d0f3d0b7008cb74d
Furqon Mauladani <furqonmauladani@gmail.com>
2022-02-02 21:31:53 +0700

    Update application and deployment information

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html',msg="Hello World")
 
+@app.route('/_ah/warmup')
+def warmup():
+    # Handle your warmup request logic here, e.g. set up a database connection pool
+    return "Success"
+
 if __name__=="__main__":
     app.run(host='0.0.0.0',port=80)
