8623a959906dc2e75d5f25b8ecb44ea0dd3324d0
Furqon Mauladani <furqonmauladani@gmail.com>
2024-08-29 09:00:47 +0700

    Update to support Python 3.12

--- main.py
+++ main.py
@@ -1,24 +1,10 @@
-# Copyright 2016 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+from flask import Flask
 
-import webapp2
+app = Flask(__name__)
 
-class MainPage(webapp2.RequestHandler):
-    def get(self):
-        self.response.headers['Content-Type'] = 'text/html'
-        self.response.write('<html><head><title>Demo</title></head><body><h1>Hello World 123!</h1></body></html>')
+@app.route('/')
+def hello():
+    return '<html><head><title>Demo</title></head><body><h1>Hello World!</h1></body></html>'
 
-app = webapp2.WSGIApplication([
-    ('/', MainPage),
-], debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
